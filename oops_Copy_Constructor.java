547d0360d5a61b6b6fbebebab2d48dbda67de4e2
surajxx159357 <85638363+surajxx159357@users.noreply.github.com>
2022-09-02 22:04:38 +0530

    Update oops_Copy_Constructor.java
    here we have used copy constructor i.e. 3rd type of constructor using this we can create multiple object of same type

--- oops_Copy_Constructor.java
+++ oops_Copy_Constructor.java
@@ -1,1 +1,35 @@
 
+// here we have to create Student class 
+class Student{
+    String name;
+    int age;
+    
+    // here we have created printInfo() method to print the information of student
+    
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+    
+    // here we have created copy constructer in which we have pass one constructor inside another
+    
+    Student(Student s2){
+        this.name=s2.name;
+        this.age=s2.age;
+    }
+    Student(){
+        // this empty constructor is very important to create otherwise it'll show error
+    }
+}
+
+
+public class CopyConstructor{
+    public static void main(String[]args){
+        Student s1=new Student();
+        s1.name="suraj";
+        s1.age=21;
+        
+        Student s2=new Student(s1);  // here in this line of code new object s2 will take the same property as object s1 using copy constructor
+        s2.printInfo();
+    }
+}
