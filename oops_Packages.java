f0497b045be64a58ce863b58c86c77242b8a2d26
surajxx159357 <85638363+surajxx159357@users.noreply.github.com>
2022-09-09 20:05:17 +0530

    Create oops_Packages.java
    in this file we have created package and syntax of package

---
+++ oops_Packages.java
@@ -0,0 +1,24 @@
+package Bank;
+
+/*
+ * NOTE: here we have created a package named "Bank" 
+ *       inside Bank package whatever class structure we'll create we can use
+ *       their syntax in other file by using package name (using keyword = import package_name)
+ *       we can also use package syntax inside the package
+ */
+
+class Account{
+	String name;
+}
+public class BankAccountDetails {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
+
+
+
+
