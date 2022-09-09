9cfce08023ec1d36cac2459e80d43b2d232ed71f
surajxx159357 <85638363+surajxx159357@users.noreply.github.com>
2022-09-09 20:06:37 +0530

    Create oops_PackageUse.java
    in this section we have use "Bank" package to extract the properties or syntax of classes to use in this file

---
+++ oops_PackageUse.java
@@ -0,0 +1,12 @@
+package Bank;
+
+public class Packageexample {
+
+	public static void main(String[] args) {
+
+		Bank.Account a1=new Bank.Account();
+		a1.name="suraj";
+		System.out.println(a1.name);
+	}
+
+}
