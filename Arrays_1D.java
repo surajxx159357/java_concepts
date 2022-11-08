274b5ed0847493e522bbe8b8c82d254206c325aa
surajxx159357 <85638363+surajxx159357@users.noreply.github.com>
2022-11-08 20:26:10 +0530

    Create Arrays_1D.java
    In the given program we'll see how to print the months name

---
+++ Arrays_1D.java
@@ -0,0 +1,16 @@
+
+// printing the months 
+
+
+public class One_D_Arrays{
+    public static void main(String []args){
+        
+        // String[] month=new String[12];  // Declaration and Creation
+        // month[1]="january";            //  Initialization
+        
+        String[] month= {"january","february","march","april","may","june","july","august","September","october","november","december"}; // Declartion, creation and initialization
+        
+        for(int i=0;i<month.length;i++)
+        System.out.println(month[i]);  // retrieving the values of an arrays 
+    }
+}
