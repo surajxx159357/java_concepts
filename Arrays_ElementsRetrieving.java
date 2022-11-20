8d2bb91db54f8ef5fdb8ab008e425914b040556f
surajxx159357 <85638363+surajxx159357@users.noreply.github.com>
2022-11-20 20:31:13 +0530

    Create Arrays_ElementsRetrieving.java
    in this section we'll see how to retrieve elements of an arrays using different methods

---
+++ Arrays_ElementsRetrieving.java
@@ -0,0 +1,39 @@
+public class ArraysAnonymous{
+    public static void main(String[]args){
+        
+    int [] num={10,30};
+  
+    //using for loop
+    
+    // for(int i=0;i<num.length;i++){
+    //     System.out.println(num[i]);
+    // }
+  
+  
+  //using for each loop
+  
+//   for(int i:num){
+//       System.out.print(i+" ");
+//   }
+  
+  
+    //using while loop
+    
+//     int i=0;
+//     while(i<=1){
+//     System.out.print(num[i]+" ");
+//     i++;
+// }
+
+
+
+    //using do while loop
+    
+    // int i=0;
+    // do{
+    //     System.out.print(num[i]+" ");
+    //     i++;
+    // }while(i<num.length);
+    
+    }    
+}
